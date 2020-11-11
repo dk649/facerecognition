0d3fde3c24c18f54431f166769a5ec8a06405d04
dk649 <58308212+dk649@users.noreply.github.com>
2020-11-11 13:16:36 +0000

    Updates

---
+++ precache-manifest.08850c86135d37b2f6e647f427b95e05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93b209845ba7a4ed95ed8df333d995cb",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "996ff93d5da135b20ba3",
+    "url": "/facerecognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4596498a0e6a8da3f351",
+    "url": "/facerecognition/static/css/main.1ce2b6bf.chunk.css"
+  },
+  {
+    "revision": "996ff93d5da135b20ba3",
+    "url": "/facerecognition/static/js/2.6fb84acc.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facerecognition/static/js/2.6fb84acc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4596498a0e6a8da3f351",
+    "url": "/facerecognition/static/js/main.e32886be.chunk.js"
+  },
+  {
+    "revision": "fa6a11a9cd7eebd80802",
+    "url": "/facerecognition/static/js/runtime-main.1ad3eaac.js"
+  }
+]);
